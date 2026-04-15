94650dbdf761a287a8fec5258f33a2efe18bb2d8
sborodich <stanislau.barodzich@innowise.com>
2026-04-15 21:54:20 +0300

    #

--- DZ_2/DZ_2_2/index.js
+++ DZ_2/DZ_2_2/index.js
@@ -6,8 +6,8 @@ let gold = 0;
 while(energy > 0){
     energy -= 1;
     depth += 1;
-    if(depth==7){
-        console.log(`\nМонстр! Гном в панике убегает!`);
+    if(depth===7){
+        console.log(`\nМонстр! Гном в панике убегает!`)
         break};
     if(depth%3==0){ gold+=1};
     console.log(`\n
